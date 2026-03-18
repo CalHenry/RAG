82d656d913cac87bcbd2e54911f9beeb6b15792a
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-03-18 21:34:44 +0100

    feat: data_models
    RAGDeps arg embedder has now the correct data type (SentenceTransformer)
    Add a classmethod to create a RAGDeps object
    add docstring to the create() method

--- src/rag/data_models.py
+++ src/rag/data_models.py
@@ -5,6 +5,9 @@ from typing import Annotated, Any, Optional
 import lancedb
 from lancedb.pydantic import LanceModel, Vector
 from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
+
+from config import DB_PATH, TABLE_NAME
 
 
 # ingestion pipeline
@@ -33,7 +36,23 @@ class RAGResponse(BaseModel):
 class RAGDeps:
     db: lancedb.DBConnection
     table_name: str
-    embedder: Any
+    embedder: SentenceTransformer
     retrieval_query: str
     top_k: int = 5
     doc_id: int
+
+    @classmethod
+    def create(
+        cls, embedder: SentenceTransformer, retrieval_query: str, doc_id: int
+    ) -> "RAGDeps":
+        """Factory method for RAGDeps. db and table_name are set in config.py"""
+        return cls(
+            db=lancedb.connect(DB_PATH),
+            table_name=TABLE_NAME,
+            embedder=embedder,
+            retrieval_query=retrieval_query,
+            doc_id=doc_id,
+        )
+
+
+RAGDeps.create
