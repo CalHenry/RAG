10e24b6f0dc7403029ded8d666ba8e1352aed379
Calixte Henry <131826512+CalHenry@users.noreply.github.com>
2026-03-19 23:15:04 +0100

    feat:
    env var for open router api key
    add support to read from .env file

--- src/rag/config.py
+++ src/rag/config.py
@@ -1,10 +1,19 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Embedding model -----------------------------------------------------------
 MODEL_PATH = "./models/bge-large-zh-v1.5"
 MODEL_NAME = "BAAI/bge-large-zh-v1.5"
 
-# query pipeline variables --------------------------------------------------
+# query pipeline  --------------------------------------------------
+load_dotenv()
+
+USE_OPENROUTER = os.getenv("USE_OPENROUTER", "false").lower() == "true"
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")  # None if not set
+
+
 BASE = Path(__file__).parent.parent.parent  # := ./src/rag/
 
 # Key words to match against the vectors in the vector database. Also added to the system prompt
